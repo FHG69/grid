89531314bce4e2bb39ae530d23d974ebdb3df9b0
FHG69 <105478523+FHG69@users.noreply.github.com>
2022-06-07 20:23:42 +0200

    Delete includes directory

--- includes/db/db.php
+++
@@ -1,5 +0,0 @@
-<?PHP
-$serverName = "DESKTOP-PBIOREA";//serverName\instanceName
-$connectionInfo = array( "Database"=>"pendel_db", "UID"=>"pendel", "PWD"=>"frank");
-$conn = sqlsrv_connect( $serverName, $connectionInfo);
-?>
